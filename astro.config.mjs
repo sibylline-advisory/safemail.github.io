8a4e2626ff1b39554fcd3f2027325f50426d45c8
aMacDawg <andrewmmcintosh@gmail.com>
2023-08-13 00:44:14 -0700

    update astro.config

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,7 +2,7 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-    site: "https://safemail.github.io"
+    site: "https://aMacDawg.github.io"
     //base: "/safemail"
     // commenting out because name of the repo is <sub>.github.io as documented here:
     // https://docs.astro.build/en/guides/deploy/github/
