41b3add7502c7f2a43b459bd1eb6cd7a1999fd90
aMacDawg <andrewmmcintosh@gmail.com>
2023-08-13 23:46:01 -0700

    manual install tailwind

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,7 +3,8 @@ import { defineConfig } from 'astro/config';
 // https://astro.build/config
 export default defineConfig({
     site: "https://safemail.github.io",
-    base: "/safemail.github.io"
+    base: "/safemail.github.io",
     // commenting out because name of the repo is <sub>.github.io as documented here:
     // https://docs.astro.build/en/guides/deploy/github/
+    integrations: [tailwind()],
 });
