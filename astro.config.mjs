af6bbc9ff32da5c1d14d49e05d711e8ab6bece08
aMacDawg <andrewmmcintosh@gmail.com>
2023-08-23 01:25:49 -0700

    updating config file for ext domain

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,8 +3,8 @@ import tailwind from '@astrojs/tailwind';
 
 // https://astro.build/config
 export default defineConfig({
-    site: "https://safemail.github.io",
-    base: process.env.ASTRO_ENV === 'development' ? '/' : '/safemail.github.io',
+    site: "https://safemail.ai",
+    base: process.env.ASTRO_ENV === 'development' ? '/' : '/',
     // commenting out because name of the repo is <sub>.github.io as documented here:
     // https://docs.astro.build/en/guides/deploy/github/
     integrations: [tailwind()],
